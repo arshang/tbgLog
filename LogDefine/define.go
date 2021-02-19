8c7223549656ab00d735c1043b801e981b5362de
arshang <arshang1007@gmail.com>
2021-02-19 14:29:05 +0800

    Add LogDefine/Struct

---
+++ LogDefine/define.go
@@ -0,0 +1,11 @@
+package LogDefine
+
+const (
+    GAME_NORMAL = iota
+    GAME_FREE
+)
+
+const (
+    AWARD_NONE = iota
+    AWARD_CHEST
+)
---
+++ LogStruct/gameLog.go
@@ -0,0 +1,33 @@
+package LogStruct
+
+import (
+	"time"
+)
+
+type GameLog struct {
+	ArcadeID   int
+	GameType   int
+	InsertCoin int
+	WinCoin    int
+	ChanceL    int
+	ChanceR    int
+	ChestAward int
+	Time       int
+}
+
+func NewLog(arcadeID int, insertCoin int, winCoin int, chanceL int, chanceR int, chestAward int, gameType int) *GameLog {
+
+	timeSecond := time.Now().Unix()
+	log := &GameLog{
+		ArcadeID:   arcadeID,
+		GameType:   gameType,
+		InsertCoin: insertCoin,
+		WinCoin:    winCoin,
+		ChanceL:    chanceL,
+		ChanceR:    chanceR,
+		ChestAward: chestAward,
+		Time:       int(timeSecond),
+	}
+
+	return log
+}
